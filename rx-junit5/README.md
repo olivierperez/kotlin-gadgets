9f3452d90c873002c2ee9588e486634f1bdf276e
Olivier Perez [a570709] <olivier.perez@worldline.com>
2018-05-23 13:16:11 +0000

    Create new gadget - RxExtension for Junit5

---
+++ rx-junit5/README.md
@@ -0,0 +1,12 @@
+# Junit5 Jupiter extension for RxJava2
+
+Give a look at [RxExtension](RxExtension.kt) and [Example](example.kt).
+
+```kotlin
+@ExtendWith(value = [RxExtension::class])
+class ExampleUnitTest() {
+
+    /* ... */
+
+}
+```
---
+++ rx-junit5/RxExtension.kt
@@ -0,0 +1,23 @@
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.jupiter.api.extension.AfterAllCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.mockito.Mockito
+
+class RxExtension : BeforeAllCallback, AfterAllCallback {
+
+    override fun beforeAll(context: ExtensionContext) {
+        RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setNewThreadSchedulerHandler { Schedulers.trampoline() }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+    }
+
+    override fun afterAll(context: ExtensionContext) {
+        RxJavaPlugins.reset()
+        RxAndroidPlugins.reset()
+    }
+
+}
